2be27c5696d868c21562491087ae187617cdf4ac
Peter van Dooren <p.v.dooren@tue.nl>
2025-06-11 16:56:43 +0200

    add tests for varying input types

--- test_variablebindings_geometric.py
+++ test_variablebindings_geometric.py
@@ -8,13 +8,109 @@ from shapely import Polygon, overlaps
 
 class TestVariableBindingsGeometric(unittest.TestCase):
 
-    def test_unify(self):
+    def test_unify_area_area(self):
+        """
+        test unification of two predefined areas
+        """
+        vb = VariableBindingsGeometric()
+        
+        area_args = {"base": Argument(),
+                     "A": Argument(),
+                     "B": Argument(),
+                     "C": Argument()}
+        areas = {}
+        areas[area_args["base"]] = Polygon([(0, 0), (0, 1), (1, 1), (1, 0)])
+        areas[area_args["A"]] = Polygon([(0, 0), (0, 0.5), (0.5, 0.5), (0.5, 0)])
+        areas[area_args["B"]] = Polygon([(0, 0), (0, 0.2), (0.2, 0.2), (0.2, 0)]) # fits in A
+        areas[area_args["C"]] = Polygon([(0, 0.3), (0, 0.7), (0.4, 0.7), (0.4, 0.3)]) # does not fit in A
+        vb.set_areas(areas)
+        vb.set_base_area(area_args["base"])
+
+        objects = {"A": Argument()}
+
+        self.assertTrue(vb.unify(area_args["B"], area_args["A"]), "within(A,B) could not be added")
+        self.assertFalse(vb.unify(area_args["C"], area_args["A"]), "within(A,C) could be added")
+        self.assertTrue(vb.resolve(), "could not resolve variablebindings")
+
+    def test_unify_area_variable(self):
+        """
+        test unification of a predefined area with a variable
+        """
         vb = VariableBindingsGeometric()
 
+        area_args = {"base": Argument(),
+                     "A": Argument(),
+                     "B": Argument(),
+                     "C": Argument()}
         areas = {}
-        areas['base'] = Polygon([(0, 0), (0, 1), (1, 1), (1, 0)])
+        areas[area_args["base"]] = Polygon([(0, 0), (0, 1), (1, 1), (1, 0)])
+        areas[area_args["A"]] = Polygon([(0, 0), (0, 0.5), (0.5, 0.5), (0.5, 0)])
+        areas[area_args["B"]] = Polygon([(0, 0), (0, 0.2), (0.2, 0.2), (0.2, 0)]) # fits in A
+        areas[area_args["C"]] = Polygon([(0, 0.3), (0, 0.7), (0.4, 0.7), (0.4, 0.3)]) # does not fit in A
         vb.set_areas(areas)
-        vb.set_base_area('base')
+        vb.set_base_area(area_args["base"])
+
+        objects = {"A": Argument()}
+        variables = {"A": Argument(),
+                     "B": Argument(),
+                     "C": Argument()}
+
+        vb.register_variable(variables["A"], objects["A"], 0.2, 0.2)
+
+        self.assertTrue(vb.unify(area_args["A"], variables["A"]), "within(area,var) could not be added")
+        self.assertTrue(vb.resolve(), "could not resolve variablebindings")
+
+        area_A = vb.placelocs[variables["A"]].area_assigned
+        self.assertIsNotNone(area_A)
+
+    def test_unify_variable_area(self):
+        """
+        test unification of a variable with a predefined areas
+        """
+        vb = VariableBindingsGeometric()
+
+        area_args = {"base": Argument(),
+                     "A": Argument(),
+                     "B": Argument(),
+                     "C": Argument()}
+        areas = {}
+        areas[area_args["base"]] = Polygon([(0, 0), (0, 1), (1, 1), (1, 0)])
+        areas[area_args["A"]] = Polygon([(0, 0), (0, 0.5), (0.5, 0.5), (0.5, 0)])
+        areas[area_args["B"]] = Polygon([(0, 0), (0, 0.2), (0.2, 0.2), (0.2, 0)]) # fits in A
+        areas[area_args["C"]] = Polygon([(0, 0.3), (0, 0.7), (0.4, 0.7), (0.4, 0.3)]) # does not fit in A
+        vb.set_areas(areas)
+        vb.set_base_area(area_args["base"])
+
+        objects = {"A": Argument()}
+        variables = {"A": Argument(),
+                     "B": Argument(),
+                     "C": Argument()}
+
+        vb.register_variable(variables["A"], objects["A"], 0.2, 0.2)
+
+        self.assertTrue(vb.unify(variables["A"], area_args["A"]), "within(var,area) could not be added")
+        self.assertTrue(vb.resolve(), "could not resolve variablebindings")
+
+        area_A = vb.placelocs[variables["A"]].area_assigned
+        self.assertIsNotNone(area_A)
+
+    def test_unify_variable_variable(self):
+        """
+        test unification of two predefined areas
+        """
+        vb = VariableBindingsGeometric()
+
+        area_args = {"base": Argument(),
+                     "A": Argument(),
+                     "B": Argument(),
+                     "C": Argument()}
+        areas = {}
+        areas[area_args["base"]] = Polygon([(0, 0), (0, 1), (1, 1), (1, 0)])
+        areas[area_args["A"]] = Polygon([(0, 0), (0, 0.5), (0.5, 0.5), (0.5, 0)])
+        areas[area_args["B"]] = Polygon([(0, 0), (0, 0.2), (0.2, 0.2), (0.2, 0)]) # fits in A
+        areas[area_args["C"]] = Polygon([(0, 0.3), (0, 0.7), (0.4, 0.7), (0.4, 0.3)]) # does not fit in A
+        vb.set_areas(areas)
+        vb.set_base_area(area_args["base"])
 
         objects = {"A": Argument()}
         variables = {"A": Argument(),
@@ -35,10 +131,17 @@ class TestVariableBindingsGeometric(unittest.TestCase):
     def test_disjunct(self):
         vb = VariableBindingsGeometric()
 
+        area_args = {"base": Argument(),
+                     "A": Argument(),
+                     "B": Argument(),
+                     "C": Argument()}
         areas = {}
-        areas['base'] = Polygon([(0, 0), (0, 1), (1, 1), (1, 0)])
+        areas[area_args["base"]] = Polygon([(0, 0), (0, 1), (1, 1), (1, 0)])
+        areas[area_args["A"]] = Polygon([(0, 0), (0, 0.5), (0.5, 0.5), (0.5, 0)])
+        areas[area_args["B"]] = Polygon([(0, 0), (0, 0.2), (0.2, 0.2), (0.2, 0)]) # fits in A
+        areas[area_args["C"]] = Polygon([(0, 0.3), (0, 0.7), (0.4, 0.7), (0.4, 0.3)]) # does not fit in A
         vb.set_areas(areas)
-        vb.set_base_area('base')
+        vb.set_base_area(area_args["base"])
 
         objects = {"A": Argument(),
                    "B": Argument()}
