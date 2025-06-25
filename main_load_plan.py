06e0474e37f6bd23ab90cf6b97113307350ba31d
Peter van Dooren <p.v.dooren@tue.nl>
2025-06-25 16:19:54 +0200

    load any plan and check

---
+++ main_load_plan.py
@@ -0,0 +1,13 @@
+import sys
+from GPlan import GPlan
+
+if __name__ == '__main__':
+    num_args = len(sys.argv)
+    if num_args >1:
+        json_file = sys.argv[1]
+    else:
+        json_file = 'plan_1.json'
+
+    # Load the plan from the JSON file
+    plan = GPlan.from_json(json_file)
+    plan.check_plan()
