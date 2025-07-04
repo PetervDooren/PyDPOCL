865030cb2ad82d1aa37028a2c43c0ca948ba4d68
Peter van Dooren <p.v.dooren@tue.nl>
2025-07-04 14:01:05 +0200

    benchmark ark-domain

--- tests/test_benchmarks.py
+++ tests/test_benchmarks.py
@@ -17,6 +17,20 @@ class TestBenchmarks(unittest.TestCase):
         self.assertGreater(len(plans), 0, "No plans found")
         for plan in plans:
             self.assertTrue(plan.check_plan(), "Plan is not valid")
+    
+    def test_ark_benchmark(self):
+        domain_file = 'tests/benchmarks/ark-domain/ark-domain.pddl'
+        problem_file = 'tests/benchmarks/ark-domain/ark-problem.pddl'
+        worldmodel_file = None  # ark problem has no geometric variables
+
+        # load domain and problem
+        domain, problem = load_domain_and_problem(domain_file, problem_file, worldmodel_file)
+
+        planner = POCLPlanner(domain, problem)
+        plans = planner.solve(k=1)
+        self.assertGreater(len(plans), 0, "No plans found")
+        for plan in plans:
+            self.assertTrue(plan.check_plan(), "Plan is not valid")
 
 if __name__ == '__main__':
     unittest.main()
