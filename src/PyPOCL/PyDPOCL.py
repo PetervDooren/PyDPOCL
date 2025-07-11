24ebdf4af3bb020d25ad3f2e5f924b7bddf28da9
Peter van Dooren <p.v.dooren@tue.nl>
2025-07-11 11:21:15 +0200

    add print statement

--- src/PyPOCL/PyDPOCL.py
+++ src/PyPOCL/PyDPOCL.py
@@ -188,6 +188,7 @@ class POCLPlanner:
 
 			if len(plan.flaws) == 0:
 				if plan.variableBindings.symbolic_vb.is_fully_ground():
+					print("attempting to resolve the geometric CSP")
 					if plan.variableBindings.geometric_vb.resolve():
 						plan.solved = True
 						# success
