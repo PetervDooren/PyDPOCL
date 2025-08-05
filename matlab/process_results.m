fad969e710b94a3b5506e18ab1aeded92617aaf6
Peter van Dooren <p.v.dooren@tue.nl>
2025-08-05 15:32:42 +0200

    test results

--- matlab/process_results.m
+++ matlab/process_results.m
@@ -8,8 +8,11 @@ noPlanFoundRows = data(strcmp(data.status, 'no_plan_found'), :);
 FaultyRows = data(strcmp(data.status, 'faulty_plan'), :);
 
 
-% Extract rows where the status field is 'faulty_plan'
+% Extract rows where the status field is 'success'
 SuccessRows = data(strcmp(data.status, 'success'), :);
+
+% Extract rows that took a long planning time'
+LongPlanningTimeRows = data(SuccessRows.planning_time > 10, :);
 
 figure(1)
 plot(SuccessRows.nr_goals, SuccessRows.planning_time, 'o')
