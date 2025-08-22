e278ed0182ba7323f9d76bf2943e2c84084ae248
Peter van Dooren <p.v.dooren@tue.nl>
2025-08-22 16:48:30 +0200

    update main input

--- main.py
+++ main.py
@@ -7,10 +7,16 @@ LOG = 1
 
 if __name__ == '__main__':
     num_args = len(sys.argv)
-    if num_args >1:
+    if num_args == 2:
+        test_i = sys.argv[1]
+        domain_file = 'domains/manipulation-domain/manipulation-domain.pddl'
+        problem_file = f'domains/manipulation-domain-batch/test_{test_i}_problem.pddl'
+        worldmodel_file = f'domains/manipulation-domain-batch/test_{test_i}_worldmodel.json'
+    elif num_args > 2:
         domain_file = sys.argv[1]
-        if num_args > 2:
-            problem_file = sys.argv[2]
+        problem_file = sys.argv[2]
+        if num_args > 3:
+            worldmodel_file = sys.argv[2]
     else:
         #domain_file = 'domains/ark-domain/ark-domain.pddl'
         #problem_file = 'domains/ark-domain/ark-problem.pddl'
